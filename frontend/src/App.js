bf5b6c46b1ae2c76ceb2bfe4bd129b8c12339795
zuhye <mari5669@naver.com>
2023-05-06 22:13:03 +0900

    Init: 초기ì세팅

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+    );
+}
+
+export default App;
