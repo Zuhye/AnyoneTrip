f485796ca5c8b5463a9838746af5d5f9779c0f75
NoMaL21 <dntjd8509@naver.com>
2023-05-17 19:07:03 +0900

    [fix] 그리드 형식으로 고침

--- frontend/src/pages/PlanLocation.jsx
+++ frontend/src/pages/PlanLocation.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect } from 'react';
 import Header from '../components/Header.jsx';
 
+import '../css/planLocation.css';
+
 const {kakao} = window;
 
 function PlanLocation() {
-    const mapStyles = {
-        width: '500px',
-        height: '400px'
-    };
 
     useEffect(() => {
         const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
@@ -19,10 +17,26 @@ function PlanLocation() {
         const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
     }, []); // 빈 배열을 두번째 인자로 전달하여, 컴포넌트가 마운트될 때만 실행되도록 함.
 
+    var mapobject = (
+        <div id="map" className="map"></div>
+    )
+
+    var page = (
+        <div className="container">
+          <div id="item1">내용1</div>
+          <div className="row">
+            <div id="item2">
+              {mapobject}
+            </div>
+            <div id="item3">내용2-2</div>
+          </div>
+        </div>
+      );
+
     return (
         <div>
             <Header />
-            <div id="map" style={mapStyles}></div>
+            {page}
         </div>
     );
 }
