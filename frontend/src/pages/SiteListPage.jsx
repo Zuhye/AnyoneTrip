a1e36eb53757fa736e18caf6db4bd84ab9106510
zuhye <mari5669@naver.com>
2023-05-24 23:15:22 +0900

    feat: contenttypeid별 필터링

--- frontend/src/pages/SiteListPage.jsx
+++ frontend/src/pages/SiteListPage.jsx
@@ -1,24 +1,20 @@
 import {useEffect, useState} from 'react';
-
-import '../css/allList.css';
-import SearchIcon from '@mui/icons-material/Search';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
 import SearchBar from '../components/SearchBar.jsx';
 import Select from 'react-select';
-
-import HeartImg from '../image/like.png';
-import EmptyHeart from '../image/heart.png';
 import * as API from "../API/userApi.jsx";
 
 import '../css/allList.css';
 
 
 
-function SiteListPage(){
+function SiteListPage() {
 
     const [search, setSearch] = useState("");
     const [site, setSite] = useState([]);
+    const [selectedSido, setSelectedSido] = useState('');
+    const [filteredSite, setFilteredSite] = useState([]);
 
     useEffect(()=> {
         const siteData = async()=> {
@@ -26,7 +22,7 @@ function SiteListPage(){
                 await API.get('/data/areabased.json').then((res)=> {
                     if(res.data) {
                         setSite(res.data.site);
-                        console.log("1", res.data.site);
+                        setFilteredSite(res.data.site);
                     }
                 })
             } catch(e) {
@@ -36,48 +32,75 @@ function SiteListPage(){
         siteData();
     }, []);
 
+
+    const filterSite = (searchText, selectedSido, contenttypeid) => {
+        let filteredData = site;
+
+        if(searchText) {
+            filteredData = filteredSite.filter((s)=> s.title.includes(searchText));
+        }
+
+        if(selectedSido) {
+            filteredData = filteredData.filter((s)=> s.addr1.includes(selectedSido));
+        }
+
+        if(contenttypeid != "") {
+            filteredData = filteredData.filter((s)=> s.contenttypeid === contenttypeid);
+        }
+
+        return filteredData;
+    };
+
+
+
     const onSearch = async (e) => {
-        console.log(search);
         e.preventDefault();
-        const filterData = site.filter((s)=> {
-            return s.title.includes(search);
-        });
-        setSite(filterData);
-    }
+        setFilteredSite(filterSite(search, selectedSido, ""));
+    };
+
+
 
     const onChangeSearch = (e)=> {
-        e.preventDefault();
         const inputValue = e.target.value;
         setSearch(inputValue);
+
         if(inputValue.trim()=== '') {
-            try {
-                API.get('/data/areabased.json').then((res) => {
-                  if (res.data) {
-                    setSite(res.data.site);
-                  }
-                })
-              } catch (e) {
-                console.error(e);
-              }
+           setFilteredSite(filterSite("", selectedSido, ""));
         }
     };
+
+
+    const onSidoChange = (option)=> {
+        setSelectedSido(option.value);
+
+        if(option.value === '') {
+            setFilteredSite(filterSite(search, "", ""));
+        } else {
+           setFilteredSite(filterSite(search, option.label, ""));
+        }
+    };
+
+    const handleFilterChange = (contenttypeid)=> {
+        setFilteredSite(filterSite(search, selectedSido, contenttypeid));
+    }
 
     return (
         <div>
             <Header/>
             <SearchBar search={search} onSearch = {onSearch} onChangeSearch = {onChangeSearch}/>
             <div>
-                <SelectBox/>
-                <Filtering/>
-                <CardSection site = {site}/>
+                <SelectBox onSidoChange = {onSidoChange} />
+                <Filtering onFilterChange={handleFilterChange}/>
+                <CardSection site = {filteredSite} />
             </div>
             <Footer/>
         </div>
     )
 }
 
-function SelectBox(){
+function SelectBox({onSidoChange}){
     const sido = [
+    {value: '', label: '전체'},
     {value: "Seoul", label: "서울특별시"},
     {value: "Incheon", label: "인천광역시"},
     {value: "Daejeon", label: "대전광역시"},
@@ -94,19 +117,22 @@ function SelectBox(){
     {value: "Jeonnam", label: "전라남도"},
     {value: "Gyeongbuk", label: "경상북도"},
     {value: "Gyeongnam", label: "경상남도"},
-    {value: "Jejudo", label: "제주도"}
+    {value: "Jeju", label: "제주"}
 ]
 
     const [selectSido, setSelectSido] = useState(sido[0]);
 
+    const handleSidoChange = (option)=> {
+        setSelectSido(option);
+        onSidoChange(option);
+    }
+
 	return (
         <Select options={sido}
-        onChange={(e)=> setSelectSido(e.value)}
+        onChange={handleSidoChange}
         placeholder="시/도 선택"
         className='selectBox'
-        value={sido.filter(function (option) {
-            return option.value === selectSido;
-        })}
+        value= {selectSido.value}
         />
 	);
 }
@@ -127,14 +153,32 @@ function CardSection({site}) {
     )
 }
 
-function Filtering() {
+function Filtering({site, onFilterChange}) {
+    //12:관광지, 14:문화시설, 15:축제공연행사, 25:여행코스, 28:레포츠, 32:숙박, 38:쇼핑, 39:음식점
+    const contenttype = {
+        "12": "관광지",
+        "14": "문화시설",
+        "15": "축제공연행사",
+        "25": "여행코스",
+        "28": "레포츠",
+        "32": "숙박",
+        "38": "쇼핑",
+        "39": "음식점",
+      };
+    
+    const handleFilterChange = (contenttypeid)=> {
+        onFilterChange(contenttypeid);
+    }
+
     return (
         <div className='filtering_container'>
-            <button>전체보기</button>
-            <button>관광지</button>
-            <button>맛집</button>
-            <button>숙소</button>
-            <button>문화시설</button>
+            <button onClick={()=> handleFilterChange("")}>전체보기</button>
+            {Object.keys(contenttype).map((id)=> (
+            <button key = {id} onClick={()=> handleFilterChange(id)}>
+                {contenttype[id]}
+            </button>
+            ))}
+
         </div>
     )
 }
