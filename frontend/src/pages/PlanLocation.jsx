2a3595e307d80dc347b9a0e9ee4ca08c6010ec0a
NoMaL21 <dntjd8509@naver.com>
2023-05-31 18:55:53 +0900

    [feat] 지도상 오버레이 클릭으로 리스트에 추가하는 기능 구현

--- frontend/src/pages/PlanLocation.jsx
+++ frontend/src/pages/PlanLocation.jsx
@@ -4,6 +4,7 @@ import Header from '../components/Header.jsx';
 import '../css/planLocation.css';
 
 const { kakao } = window;
+window.map = null;
 
 function PlanLocation() {
   const [barrierfreeInfo, setBarrierfreeInfo] = useState([
@@ -104,32 +105,44 @@ function PlanLocation() {
   const [selectedMarker, setSelectedMarker] = useState(null);
 
   // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
-  var markers = [];
+  var markers = []; 
 
   // 별도의 객체를 활용하여 마커와 관련 정보를 저장합니다
   const [markerInfoMap, setMarkerInfoMap] = useState({});
 
   const [overlayOpen, setOverlayOpen] = useState({});
   
+  const [selectedItems, setSelectedItems] = useState([]); //선택한 관광지 요소
+
   useEffect(() => {
-    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    var options = { //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(35.8780669221, 128.6294594533), //지도의 중심좌표. y,x 순서로 적으면됨
-      level: 7 //지도의 레벨(확대, 축소 정도)
+    // map 객체를 전역 변수로 설정
+    if(window.map == null)
+    {
+      var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+      var options = { //지도를 생성할 때 필요한 기본 옵션
+        center: new kakao.maps.LatLng(35.8780669221, 128.6294594533), //지도의 중심좌표. y,x 순서로 적으면됨
+        level: 7 //지도의 레벨(확대, 축소 정도)
+      };
+  
+      const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+  
+      barrierfreeInfo.forEach((info) => {
+        const { mapx, mapy, title, contentid, firstimage, addr1 } = info;
+        const position = new kakao.maps.LatLng(parseFloat(mapy), parseFloat(mapx)); //카카오맵 LatLng는 y,x 식으로 넣어야함
+        addMarker(map, position, title, contentid, firstimage, addr1 );
+      })
+      window.map = map;
+    }
+    
+    // 마커 오버레이 내부의 "+" 버튼에 대한 클릭 이벤트 리스너
+    document.addEventListener('click', handleAddButtonClick);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거하는 클린업 함수
+    return () => {
+      document.removeEventListener('click', handleAddButtonClick);
     };
 
-    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-
-    barrierfreeInfo.forEach((info) => {
-      const { mapx, mapy, title, contentid, firstimage, addr1 } = info;
-      const position = new kakao.maps.LatLng(parseFloat(mapy), parseFloat(mapx)); //카카오맵 LatLng는 y,x 식으로 넣어야함
-      addMarker(map, position, title, contentid, firstimage, addr1 );
-    })
-
-    // map 객체를 전역 변수로 설정
-    window.map = map;
-
-  }, []); // 빈 배열을 두번째 인자로 전달하여, 컴포넌트가 마운트될 때만 실행되도록 함.
+  }, [selectedItems]); // 빈 배열을 두번째 인자로 전달하여, 컴포넌트가 마운트될 때만 실행되도록 함.
 
   // 마커를 생성하고 지도위에 표시하는 함수입니다
   function addMarker(map, position, title, contentid, firstimage, addr1 ) {
@@ -141,19 +154,20 @@ function PlanLocation() {
 
     // 마커를 클릭했을 때 마커 위에 표시할 오버레이를 생성합니다a
     var iwContent = `<div class="wrap">
-        <div class="info">
-            <div class="title">
-                ${title}
-            </div>
-            <div class="body">
-                <div class="img">
-                    <img src="${firstimage}" width="73" height="70">
+      <div class="info">
+          <div class="title">
+              ${title}
+          </div>
+          <div class="body">
+              <div class="img">
+                  <img src="${firstimage}" width="73" height="70">
               </div>
-                <div class="desc">
-                    <div class="ellipsis">${addr1}</div>
-                </div>
-            </div>
-        </div>    
+              <div class="desc">
+                  <div class="ellipsis">${addr1}</div>
+                  <button class="add-button" data-contentid="${contentid}">+</button>
+              </div>
+          </div>
+      </div>
     </div>`; // 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
 
     // 오버레이를 생성합니다
@@ -229,7 +243,37 @@ function PlanLocation() {
 
   };
 
-  var mapobject = (
+  // 오버레이의 + 버튼 클릭 이벤트 처리 함수
+  function handleAddButtonClick(event) {
+    event.stopPropagation(); // 이벤트 전파 중단
+  
+    const contentid = event.target.dataset.contentid;
+  
+    // 이미 선택된 항목 중에 동일한 contentid가 있는지 확인
+    const isDuplicate = selectedItems.some((item) => item.contentid === contentid);
+  
+    console.log(isDuplicate);
+    console.log(selectedItems);
+    if (isDuplicate) {
+      // 이미 선택된 항목 중에 동일한 contentid가 있다면 아무 작업을 수행하지 않음
+      return;
+    }
+  
+    // 선택된 마커 정보 가져오기
+    const markerInfo = markerInfoMap[contentid];
+    if (markerInfo) {
+      const { contentid, title, overlay } = markerInfo;
+      setSelectedItems((prevItems) => [...prevItems, { title, contentid, overlay }]); // 수정된 부분: overlay도 추가
+    }
+  }
+
+  const handleCancelClick = (contentid) => {
+    setSelectedItems((prevItems) =>
+      prevItems.filter((item) => item.contentid !== contentid)
+    );
+  };
+
+  var mapobject = ( 
     <div id="map" className="map"></div>
   );
 
@@ -243,23 +287,33 @@ function PlanLocation() {
         {info.title}
       </li>
     ))
-
   );
-
-  var listobjects = (<li></>);
 
   var page = (
     <div className="container">
-      <div id="item1">
+      <div id="listcontainer">
         <ul>
           {listobject}
         </ul>
       </div>
       <div className="row">
-        <div id="item2">
+        <div id="mapcontainer">
           {mapobject}
         </div>
-        <div id="item3">내용2-2</div>
+        <div id="item3">
+          {selectedItems.map((item) => (
+            <div key={item.contentid} className="item-container">
+              <div className="item-title">{item.title}</div>
+              <button
+                className="cancel-button"
+                onClick={() => handleCancelClick(item.contentid)}
+              >
+                -
+              </button>
+            </div>
+          ))}
+          <button>확정하기</button>
+        </div>
       </div>
     </div>
   );
